e24fb24d47d8f87f617f06a4b32cf51d3b2c9bf7
Sergio Castro <sergiojuniorce@gmail.com>
2024-10-11 01:28:28 -0700

    added articles and basic auth

---
+++ src/routes/article-card.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import * as Card from '$lib/components/ui/card';
+
+  export let title = 'Card Title';
+  export let summary = 'Card Summary';
+  export let author = 'Card Author';
+  export let date = 'Card Date';
+</script>
+
+<Card.Root>
+  <Card.Header>
+    <Card.Title>{title}</Card.Title>
+  </Card.Header>
+  <Card.Content>
+    <p>{summary}</p>
+  </Card.Content>
+  <Card.Footer>
+    <p>Author: {author} - {date}</p>
+  </Card.Footer>
+</Card.Root>
