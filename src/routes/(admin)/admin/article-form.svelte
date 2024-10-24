8e3e9be6f95f54acd28fb342a27fe64550eaaea1
Sergio Castro <sergiojuniorce@gmail.com>
2024-10-24 01:08:24 -0700

    maybe

--- src/routes/(admin)/admin/article-form.svelte
+++ src/routes/(admin)/admin/article-form.svelte
@@ -2,6 +2,31 @@
 	import { Input } from '$lib/components/ui/input';
 	import Label from '$lib/components/ui/label/label.svelte';
 	import { Textarea } from '$lib/components/ui/textarea';
+	import { onMount } from 'svelte';
+
+	let editor: HTMLElement;
+
+	const toolbarOptions = [
+		[{ header: [1, 2, false] }],
+		['bold', 'italic', 'underline'],
+		['link', 'image'],
+		[{ list: 'ordered' }, { list: 'bullet' }],
+		[{ indent: '-1' }, { indent: '+1' }],
+		[{ align: [] }],
+		['clean']
+	];
+
+	onMount(async () => {
+		const { default: Quill } = await import('quill');
+
+		let quill = new Quill(editor, {
+			modules: {
+				toolbar: toolbarOptions
+			},
+			theme: 'snow',
+			placeholder: 'Write your story...'
+		});
+	});
 
 	let title = $state('title');
 	let summary = $state('summary');
